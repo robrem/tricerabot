5d2d5103d95e3665273ed72e1b39b6dc22f81340
Rob Remington <robrem@users.noreply.github.com>
2018-08-29 00:54:13 -0700

    first attempt at using IR remote to start/stop Tricerabot

--- code.py
+++ code.py
@@ -1,17 +1,18 @@
 # Tricerabot walks and roars, adapted from Adafruit industries Stumble Bot.
 # See: https://learn.adafruit.com/stumble-bot-with-circuit-playground-and-crickit
 
-import time
+import adafruit_irremote
+import audioio
 import board
+import pulseio
+import time
 from digitalio import DigitalInOut, Direction, Pull
 from adafruit_crickit import crickit
-
-# for dinosaur roar
-import audioio
 
 
 STEPS = 100                               # Number of steps to take
 SOUND_FILE = 'dino_roar.wav'              # Sound file to play
+IR_REMOTE_POWER = [255, 0, 93, 162]
 
 led = DigitalInOut(board.D13)            # Set up Red LED
 led.direction = Direction.OUTPUT
@@ -35,6 +36,12 @@ servos[0].angle = 90
 spkrenable = DigitalInOut(board.SPEAKER_ENABLE)
 spkrenable.direction = Direction.OUTPUT
 spkrenable.value = True
+
+# Create a 'pulseio' input, to listen to infrared signals on the IR receiver
+pulsein = pulseio.PulseIn(board.IR_RX, maxlen=120, idle_state=True)
+
+# Create a decoder that will take pulses and turn them into numbers
+decoder = adafruit_irremote.GenericDecode()
 
 
 def servo_front(direction):
@@ -79,23 +86,41 @@ def play_file(filename):
 
 print("It's TriceraBot Time")
 
+bot_is_on = False
+
 while True:
-    if button_A.value:     # If button A is pressed, start bot
+    pulses = decoder.read_pulses(pulsein)
+
+    try:
+        # Attempt to convert received pulses into numbers
+        received_code = decoder.decode_bits(pulses, debug=False)
+    except adafruit_irremote.IRNECRepeatException:
+        # We got an unusual short code, probably a 'repeat' signal
+        print("NEC repeat!")
+        continue
+    except adafruit_irremote.IRDecodeException as e:
+        # Something got distorted or maybe its not an NEC-type remote?
+        print("Failed to decode: ", e.args)
+        continue
+
+    if not bot_is_on and received_code == IR_REMOTE_POWER:
         led.value = True   # Turn on LED 13 to show we're gone!
         for i in range(STEPS):
             print("back 1")
-            servo_back(1)
-            time.sleep(0.100)
+            # servo_back(1)
+            # time.sleep(0.100)
             print("front 1")
-            servo_front(1)
-            time.sleep(0.100)
+            # servo_front(1)
+            # time.sleep(0.100)
             print("back 2")
-            servo_back(-1)
-            time.sleep(0.100)
+            # servo_back(-1)
+            # time.sleep(0.100)
             print("front 2")
-            servo_front(-1)
-            time.sleep(0.100)
-            if i % 5 == 0:
-                play_file(SOUND_FILE)
+            # servo_front(-1)
+            # time.sleep(0.100)
+            # if i % 5 == 0:
+            #     play_file(SOUND_FILE)
         led.value = False
+    elif bot_is_on and received_code == IR_REMOTE_POWER:
+        print("Stopping Tricerabot")
         
